03b880301b4aa9d51fb210c450db5ab689d40c76
o.afanasiev <o.afanasiev@mobidev.biz>
2016-01-15 17:50:56 +0200

    added a Notes tab with a form for a new note creating

---
+++ source/views/popup/popup.js
@@ -0,0 +1,46 @@
+RAD.view("view.popup", RAD.Blanks.View.extend({
+
+    url: 'source/views/popup/popup.html',
+    events: {
+        'click .close-dialog': 'closeDialog'
+    },
+    className: 'popup-view',
+    attributes: {
+        'data-role': 'popup-view'
+    },
+    onInitialize: function () {
+        'use strict';
+        //this.model = new Backbone.Model();
+        this.model = RAD.model('list');
+    },
+    onNewExtras: function (extras) {
+        'use strict';
+        //this.model.set({msg: extras.msg});
+        this.caller = extras.parent;
+    },
+    onEndDetach: function () {
+        "use strict";
+        var tag = this.$('#text').get(0),
+            resultString = tag ? tag.value : '';
+        //transfer data to parent
+        this.publish(this.caller, {result: resultString });
+    },
+    closeDialog: function (e) {
+        "use strict";
+
+        var field = this.el.querySelector('#content'),
+            noteText = field.value;
+
+        e.preventDefault();
+
+        if (noteText) {
+            this.model.unshift({content: noteText});
+            field.value = '';
+        }
+
+        console.log(this.model);
+
+        this.publish('navigation.popup.close', {content: this.viewID });
+    }
+
+}));
