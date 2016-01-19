4b730e4f34a4fcc467fd7ad285a057334a5eb23f
o.afanasiev <o.afanasiev@mobidev.biz>
2016-01-19 10:36:43 +0200

    Added animations to log in and log out events.

--- source/views/screen_2/tab_2/tab_2.js
+++ source/views/screen_2/tab_2/tab_2.js
@@ -2,27 +2,13 @@ RAD.view("view.screen_2_tab_2", RAD.Blanks.ScrollableView.extend({
     url: 'source/views/screen_2/tab_2/tab_2.html',
     model: RAD.model('list'),
     events: {
-        'submit': 'addToList',
-        'click .popup-show': 'showPopup',
-        //'change .notes' : 'updateNotes',
+        'click .popup-show-add': 'showAddPopup',
+        'longtap .topcoat-list__item .label' : 'showEditPopup',
         'click .remove-icon': 'removeFromList'
     },
 
-    addToList: function(e) {
-        "use strict";
-        var field = this.el.querySelector('.task-name'),
-            taskName = field.value;
 
-        e.preventDefault();
-
-        if (taskName) {
-            this.model.unshift({content: taskName, done: false});
-            field.value = '';
-        }
-        console.log(this.model);
-    },
-
-    showPopup: function (e) {
+    showAddPopup: function (e) {
         "use strict";
 
         var self = this,
@@ -31,14 +17,39 @@ RAD.view("view.screen_2_tab_2", RAD.Blanks.ScrollableView.extend({
             target = targetID ? this.el.querySelector('#'+targetID) : e.currentTarget;
 
         this.publish('navigation.popup.show', {
-            content: "view.popup",
+            content: "view.addPopup",
             target: target,
-            width: 240,
-            height: 120,
+            width: 250,
+            height: 275,
             gravity: 'bottom',
             outsideClose: false,
             extras: {
-                //msg: msg,
+                parent: self.viewID
+            }
+        });
+
+    },
+
+    showEditPopup: function (e) {
+        "use strict";
+
+        var self = this,
+            options = document.forms['popup-options'],
+            targetID = e.currentTarget.getAttribute('data-target'),
+            target = targetID ? this.el.querySelector('#'+targetID) : e.currentTarget,
+            content = e.currentTarget.innerHTML,
+            index = e.currentTarget.getAttribute('data-index');
+
+        this.publish('navigation.popup.show', {
+            content: "view.editPopup",
+            target: target,
+            width: 400,
+            height: 120,
+            gravity: 'center',
+            outsideClose: true,
+            extras: {
+                content: content,
+                index: index,
                 parent: self.viewID
             }
         });
